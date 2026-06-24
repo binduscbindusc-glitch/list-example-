4a6717d6d0fbc0ca380458f29c2d8ef2797bff07
binduscbindusc-glitch <binduscbindusc@gmail.com>
2026-06-24 16:09:57 +0530

    Create list example.py

---
+++ list example.py
@@ -0,0 +1,11 @@
+products = ["mask", "sanitizer", "shield"]
+print(products)
+products.pop()
+print(products)
+products.remove("mask")
+print(products)
+products.insert(1, "handgloves")
+print(products)
+print(len(products))
+
+
